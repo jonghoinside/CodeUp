19468900c7e648363a701d2b3698c0cb20579f60
jonghoinside <84114844+jonghoinside@users.noreply.github.com>
2021-06-15 22:41:42 +0900

    [CodeUp] 1411번 빠진 카드 찾기 문제 풀이

---
+++ CodeUp_1411/cmake-build-debug/CMakeFiles/CodeUp_1411.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeUp_1411.dir/main.c.o"
+  "CodeUp_1411"
+  "CodeUp_1411.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CodeUp_1411.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CodeUp_1411/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) {
+    int num;
+    scanf("%d", &num);
+
+    int nums[num-1];
+    for (int i = 0 ; i < num ; ++i)
+        scanf("%d", &nums[i]);
+
+    int i, j;
+    for (i = 1; i <= num; ++i) {
+        for (j = 0; j < num-1; ++j) {
+            if (nums[j] == i)
+                break;
+        }
+        if (j == num-1) {
+            printf("%d", i);
+            return 0;
+        }
+    }
+
+    return 1;
+}
